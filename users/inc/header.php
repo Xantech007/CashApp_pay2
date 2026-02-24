4f2ce17fad6ccd633fbc07340b019e1ab5b71128
Xantech007 <113646174+Xantech007@users.noreply.github.com>
2026-02-24 23:47:42 +0100

    Update header.php

--- users/inc/header.php
+++ users/inc/header.php
@@ -14,7 +14,7 @@ if(!isset($_SESSION['auth']))
   <meta charset="utf-8">
   <meta content="width=device-width, initial-scale=1.0" name="viewport">
 
-  <title>Cash App Inc. Support Program 2025</title>
+  <title>Cash App Inc. Support Program 2026</title>
   <meta content="" name="description">
   <meta content="" name="keywords">
 
