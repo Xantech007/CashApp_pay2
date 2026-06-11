2572f498e88a8e1a66dafac31106613f1b593328
Xantech007 <113646174+Xantech007@users.noreply.github.com>
2026-06-11 23:09:54 +0100

    Update index.php

--- index.php
+++ index.php
@@ -902,7 +902,7 @@ include('includes/navbar.php');
                         <li>
                             <p>
                                 <i class="fas fa-globe-americas"></i>
-                                www.pay1.cashapp.ct.ws
+                                www.pay1.cashapp.gt.tc
                             </p>
                         </li>
                     </ul>
